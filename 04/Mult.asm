531e2756fc3cabd3e0dc478dfc84b6e528517b7b
Ryosuke <yoshimoto2107@gmail.com>
2025-01-03 20:56:43 +0900

    :sparkles:fix Mult

--- 04/Mult.asm
+++ 04/Mult.asm
@@ -8,3 +8,46 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+// R1(target)をR0(n)回足し合わせる方針
+
+    @R0
+    D=M
+    @n
+    M=D // n = R0
+    @R1
+    D=M
+    @target
+    M=D // target = R1    
+    @i
+    M=1 // i = 1
+    @sum
+    M=0 // sum = 0    
+
+    (LOOP)
+        @i
+        D=M
+        @n
+        D=D-M
+        @STOP
+        D;JGT // if i > n goto STOP
+
+        @sum
+        D=M
+        @target
+        D=D+M
+        @sum
+        M=D // sum = sum + target
+        @i
+        M=M+1 // i = i + 1
+        @LOOP
+        0;JMP
+    
+    (STOP)
+        @sum
+        D=M
+        @R2
+        M=D // RAM[2] = sum
+    
+    (END)
+        @END
+        0;JMP
