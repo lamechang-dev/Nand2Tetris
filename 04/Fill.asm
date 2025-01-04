9b868b38dbde33a9ff8455d28489e0413c1fe2f0
Ryosuke <yoshimoto2107@gmail.com>
2025-01-04 10:00:31 +0900

    :sparkles:

--- 04/Fill.asm
+++ 04/Fill.asm
@@ -8,4 +8,31 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+@8192
+D=A
+@pixel_num
+M=D
+
+(LOOP)
+    @KBD
+    D=M     // Read RAM[24576] into D
+    @KBD_PUSHED
+    D;JNE   // If D != 0, jump to KBD_PUSHED
+    @KBD_UNPUSHED
+    0;JMP   // If D == 0, jump to KBD_UNPUSHED
+
+(KBD_PUSHED)
+    @SCREEN // Address of the screen start (RAM[16384])
+    D=A     // Set D to SCREEN address
+    @DRAW_BLACK
+    0;JMP   // Jump to DRAW_BLACK
+
+(KBD_UNPUSHED)
+    @SCREEN // Address of the screen start (RAM[16384])
+    D=A     // Set D to SCREEN address
+    @DRAW_WHITE
+    0;JMP   // Jump to DRAW_WHITE
+
+(DRAW_BLACK)
+    @SCREEN  // SCREEN start address
+    D=A     // Set D to SCREEN address
